1e729515981217d2edb148b4db8dd0a512f5356a
TommasoPezzola <pezzola123@gmail.com>
2023-07-11 16:45:48 +0200

    feat: add types in edit project

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -15,6 +15,17 @@
         @enderror
         <input class="form-control @error('description') is-invalid @enderror" type="text" name="title" value="{{ old("title") ?? $project->title}}" required>
 
+        <label for="type_id">Tipo</label>
+        @error('type_id')
+            <div class="bg-danger-subtle rounded">{{$message}}</div>  
+        @enderror
+        <select class="form-control" name="type_id">
+            <option value="" >Nessuno</option>
+            @foreach ($types as $type)
+                <option value="{{ $type->id }}" @selected($project->type_id == $type->id)  >{{ $type->name }}</option>
+            @endforeach
+        </select>
+
         <label for="content">Contenuto</label>
         @error('content')
             <div class="bg-danger-subtle rounded">{{$message}}</div>  

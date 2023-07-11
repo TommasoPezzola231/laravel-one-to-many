aa8b22845e5a54c2d14552aa9cb6ebd053b1629a
TommasoPezzola <pezzola123@gmail.com>
2023-07-11 15:42:28 +0200

    feat: add type view

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 
     <h2 class="text-center">{{ $project->title }}</h2>
+    <h5>{{ $project->type->name }}</h5>
     <div class="w-50 my-2">
         <img src="{{ ($project->image) ? $project->image : "https://www.signfix.com.au/wp-content/uploads/2017/09/placeholder-600x400.png" }}" class="card-img-top" alt="{{ $project->title }}">
     </div>

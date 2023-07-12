26156ebb3e1071a3b1c0d8e402bbe0cc5d945f4c
TommasoPezzola <pezzola123@gmail.com>
2023-07-12 14:48:59 +0200

    fix LC

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use App\Models\Project;
+use App\Models\Type;
 
 class ProjectSeeder extends Seeder
 {
@@ -16,13 +17,15 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        $allTypes = Type::all(["id"]);
         
         for ($i=0; $i < 15; $i++) { 
             $newProject = new Project();
             $newProject->title = $faker->text(50);
             $newProject->content = $faker->text();
             $newProject->image = $faker->imageUrl();
-            $newProject->type_id = $faker->numberBetween(0, 5);
+            //$newProject->type_id = $faker->numberBetween(0, 5);
+            $newProject->type_id = $allTypes->random()->id;
             $newProject->save();
         }
 
